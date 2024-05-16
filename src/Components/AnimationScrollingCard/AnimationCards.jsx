b9c641bdf0fdef8cf5e8e49e41f9b02a58239960
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-05-16 12:04:18 +0530

    service animated cards Build pc and mobile

--- src/Components/AnimationScrollingCard/AnimationCards.jsx
+++ src/Components/AnimationScrollingCard/AnimationCards.jsx
@@ -16,22 +16,26 @@ const AnimatedCards = () => {
     const servicesCards = [
         {
             cardImage: demoImage,
-            cardHeading: "Creative Designs",
+            cardHeading: "Creative",
+            cardNormalHeading:" Designs",
             cardDescription: "Whether it be 3D-backed designs, moving elements, or cutouts, we have got it all! Just come to us with your product and let us help you bring the best creative designs to your audiences."
         },
         {
             cardImage: demoImage,
-            cardHeading: "Media Buying",
+            cardHeading: "Media ",
+            cardNormalHeading:"Buying",
             cardDescription: "For a successful and holistic marketing campaign, we have selected some of the best media platforms (outdoor, digital, cinema, social, print, radio, television, etc.) for you. As an aggregator, we recommend other vendors’ sites and have tie-ups with agencies that are best-in-town to provide you with the best strategic plan based on your budgetary requirements."
         },
         {
             cardImage: demoImage,
-            cardHeading: "Research-Based Campaign Planning",
+            cardHeading: "Research-Based ",
+            cardNormalHeading:"Campaign Planning",
             cardDescription: "Our aim is not to just sell our media but to maximise your target audience reach. With an extensive understanding of the target audience and consumer behaviour, our team guides you to create the ideal campaign for maximum impressions by conducting in-depth market & product research."
         },
         {
             cardImage: demoImage,
-            cardHeading: "Monitoring & Evaluation",
+            cardHeading: "Monitoring ",
+            cardNormalHeading:"& Evaluation",
             cardDescription: "We are among the most result-oriented OOH agencies that help you implement your campaigns with ease and stand by to keep a check on the impressions and evaluate your campaign performance. It is also our standard practice to provide you with real-time GPS photographs of your campaign."
         }
 
@@ -55,8 +59,8 @@ const AnimatedCards = () => {
                                 <div className="services-image-container">
                                     <img src={item.cardImage} alt="YellowSpot-services-image" style={{ width: "100%" }} />
                                 </div>
-                                <div style={{width:"100%"}}>
-                                    <h4>{item.cardHeading}</h4>
+                                <div className="animated-card-content-info">
+                                    <h4><span>{item.cardHeading}</span>{item.cardNormalHeading}</h4>
                                     <p>{item.cardDescription}</p>
                                 </div>
                             </div>
