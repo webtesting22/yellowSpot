6d28023a4e43cdc0e560ea2bd8be6b9a586f76be
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-05-25 13:13:51 +0530

    update navbar on different in the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,20 +10,93 @@ import HomePageNavigation from "./HomePageNavigation";
 import billboard from "../Home/iconsImages/billboard.png"
 import logo from "../Navbar/logo1.svg"
 import { TypeAnimation } from 'react-type-animation';
-import { Row, Col } from "antd";
+import { Row, Col,Drawer } from "antd";
 import ClientLogos from './ClientLogosData';
 import Homepagefirst from "../Home/Homepage1.jpg"
 import Industy from "../Home/Banners/OurEgency.png"
 import offering from "../Home/Banners/OurOffering.png"
 import client from "../Home/Banners/OurClients.png"
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 // import DemoImage from "../Home/Demo.png"
 import CountUp from 'react-countup';
 import Combinelogo from "../Navbar/CombineLogo.jpg"
+import LinkdinImg from "../Home/iconsImages/Vector.svg"
+import vector1 from "../Home/iconsImages/Vector1.svg"
+import vector2 from "../Home/iconsImages/Vector2.svg"
+import vector3 from "../Home/iconsImages/Vector3.svg"
+import vector4 from "../Home/iconsImages/Vector4.svg"
+import vector5 from "../Home/iconsImages/Vector5.svg"
+import vector6 from "../Home/iconsImages/Vector6.svg"
+import vector7 from "../Home/iconsImages/Vector7.svg"
+import vector8 from "../Home/iconsImages/Vector8.svg"
+import CombineLogo from "../Navbar/CombineLogo.jpg"
+import Contact from "../Home/iconsImages/ContactUs.svg"
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState('right');
+
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const showDrawer = () => {
+    setDrawerVisible(!drawerVisible); // Toggle the state
+  };
+
+  const onCloseDrawer = () => {
+    setDrawerVisible(false);
+  };
+
+  const onChange = (e) => {
+    setPlacement(e.target.value);
+  };
+  const onClose = () => {
+    setOpen(false);
+  }
   return (
     <>
       <div id="HomePage">
-        <HomePageNavigation />
+        <section id="home-page-header">
+
+          <Link to="/"> <img src={CombineLogo} alt="Yellow-Spot Logo" className="logo-home" style={{opacity:"0"}}/></Link>
+          <button className="menu-btn" onClick={showDrawer}>
+            <span className={`icon-transition ${drawerVisible ? 'rotate-180' : ''}`}>
+              {drawerVisible ? <CloseOutlined /> : <MenuOutlined />}
+            </span>
+
+          </button>
+
+          <Drawer
+            title={null}
+            placement={placement}
+            width={400}
+            onClose={onCloseDrawer}
+            visible={drawerVisible}
+            footer={null}
+
+          >
+            <div>
+              <div className="container-heading">
+                <h5>Menu</h5>
+                <hr style={{ margin: "8px 0px", border: "1px solid #FFED00", opacity: "1" }} />
+                <ul className="home-menu-ul">
+                  <Link to="/AboutUs"><li className="flip-animate" ><img src={vector5} alt="" />About&nbsp;US <span></span></li></Link>
+                  <Link to="/OurOfferings"><li className="flip-animate" ><img src={vector7} alt="" />Our Offerings<span></span></li></Link>
+                  <Link to="/OurClientele"><li className="flip-animate" ><img src={vector1} alt="" />Our Clientele<span></span></li></Link>
+                  <Link to="/ContactUs"><li className="flip-animate" ><img src={Contact} alt="" />ContactUs<span></span></li></Link>
+                </ul>
+              </div>
+              <div className="slite-padding"></div>
+              <div className="container-heading social-container">
+                <h5>Social</h5>
+                <hr style={{ margin: "8px 0px", border: "1px solid #FFED00", opacity: "1" }} />
+                <ul className=" social">
+                  <li className="flip-animate"><a href="https://www.instagram.com/yellowspot.in/" target="_blank"><img src={vector4} alt="" /></a></li>
+                  <li className="flip-animate"><img src={vector8} alt="" /></li>
+                  <li className="flip-animate"><img src={LinkdinImg} alt="" /></li>
+                  <li className="flip-animate"><a href="https://www.facebook.com/people/Yellow-Spot/100083305608740/" target="_blank"><img src={vector2} alt="" /></a></li>
+                </ul>
+              </div>
+            </div>
+          </Drawer>
+        </section>
       </div>
       <div className="home-page-slider">
         <Swiper
@@ -43,8 +116,8 @@ const Home = () => {
             <div className="image-overlay"></div>
             <div className="text-container-set">
               <div className="text-container second">
-                <div style={{display:"flex",justifyContent:"center",marginBottom:"10px"}}>
-                  <img src={Combinelogo} style={{width:"250px"}} alt="" />
+                <div style={{ display: "flex", justifyContent: "center", marginBottom: "10px" }}>
+                  <img src={Combinelogo} style={{ width: "250px" }} alt="" />
                   {/* <TypeAnimation
                     sequence={[
                       Same substring at the start will only be typed out once, initially
@@ -136,9 +209,9 @@ const Home = () => {
             <div className="our-products">
               <div className="animated-hover-boxes">
                 <div className="animated-boxes-five">
-                <div className='writeUpContent'>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste error doloribus quas dolore voluptate, deserunt minima quo illum velit sed!</p>
-                    </div>
+                  <div className='writeUpContent'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste error doloribus quas dolore voluptate, deserunt minima quo illum velit sed!</p>
+                  </div>
                   <div className="back-overlay"></div>
                   <div className="content-box">
                     <h4>Gantry</h4>
