77ee4d997e87060b44e1578833b060e57a48060d
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-06-12 10:18:28 +0530

    scroll down solve and live

--- src/Components/OurOfferings/OurOfferings.jsx
+++ src/Components/OurOfferings/OurOfferings.jsx
@@ -3,16 +3,16 @@ import { useLocation } from 'react-router-dom';
 import Navbar from "../Navbar/Navbar";
 import { Card, Row, Col } from 'antd';
 import { TypeAnimation } from 'react-type-animation';
-import "./ouroffering.css"
+import "./ouroffering.css";
 import SwiperHeadingIcon from "../SwiperHeadingIcon/SwiperHeadingIcon";
 import Inventory from "../InventryManagement/Inventory";
 import Footer from "../Footer/Footer";
 import HomePageNavigation from "../Home/HomePageNavigation";
 import AnimatedCards from "../AnimationScrollingCard/AnimationCards";
 import AnimatedCoverPage from "../animatedCoverPage/animatedCoverPage";
-import offering from "../AboutUs/AboutUsBanner.png"
-const OurOfferings = () => {
+import offering from "../AboutUs/AboutUsBanner.png";
 
+const OurOfferings = () => {
     const location = useLocation();
 
     useEffect(() => {
@@ -24,34 +24,27 @@ const OurOfferings = () => {
             }
         }
     }, [location]);
+
     return (
         <>
             <Navbar />
             <HomePageNavigation />
-            {/* <div className="section-padding"></div>
-            <br /> */}
             <AnimatedCoverPage backgroundImage={offering} />
             <br /><br />
-            <section id="serviceCard" className="container" >
-
+            <section id="serviceCard" className="container">
                 <SwiperHeadingIcon headingText="Our Services" />
                 <br />
                 <div className="container p-0">
-                    <div >
+                    <div>
                         <AnimatedCards />
                     </div>
-
-
                 </div>
             </section>
-
             <br /><br />
             <section id="OurProducts" className="container">
                 <SwiperHeadingIcon headingText="Our Products" />
                 <Inventory />
             </section>
-
-
             <Footer />
         </>
     );
