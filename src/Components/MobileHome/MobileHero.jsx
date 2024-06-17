0779f85c0435e180bc69725f403c1af2c905d517
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-06-17 12:54:15 +0530

    update home page carousal

--- src/Components/MobileHome/MobileHero.jsx
+++ src/Components/MobileHome/MobileHero.jsx
@@ -80,7 +80,7 @@ const MobileHero = () => {
                                         duration={10}
                                         suffix="+"
                                     /> */}
-                                    <h3 style={{ fontSize: "0.8rem",marginBottom:"5px" }}>xyz</h3>
+                                    <h3 style={{ fontSize: "0.8rem", marginBottom: "5px" }}>xyz</h3>
                                     <h3>Daily Impressions</h3>
                                 </div>
                             </Col>
@@ -116,84 +116,173 @@ const MobileHero = () => {
                     <div>
                         <div className="our-products">
                             <div className="animated-hover-boxes">
-                                {/* <div className='topTwoBoxes'>  */}
-                                <div className="animated-boxes-five">
-                                    <div className='doubleImageContainer'>
-                                        <div style={{
-                                            display: "flex",
-                                            zIndex: "-1",
-                                            /* position: absolute; */
-                                            width: "100%",
-                                            flexDirection: "column"
-                                        }}>
-                                            <img src={img1} alt="" style={{ border: "1px solid black", borderBottom: "none" }} />
-                                            <img src={img2} alt="" style={{ border: "1px solid black", borderBottom: "none" }} />
+                                <Swiper
+                                    slidesPerView={1}
+                                    spaceBetween={30}
+                                    // freeMode={true}
+                                    loop={true}
+                                    pagination={{
+                                        clickable: true,
+                                    }}
+                                    autoplay={{
+                                        delay: 2000,
+                                        disableOnInteraction: true,
+                                    }}
+
+                                    style={{ width: "100%", }}
+                                    modules={[FreeMode, Pagination, Autoplay]}
+                                    className="mySwiper"
+                                >
+
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className="animated-boxes-five">
+                                            <div className='doubleImageContainer'>
+                                                <div style={{
+                                                    display: "flex",
+                                                    zIndex: "-1",
+                                                    /* position: absolute; */
+                                                    width: "100%",
+                                                    flexDirection: "column"
+                                                }}>
+                                                    <img src={img1} alt="" style={{ border: "1px solid black", borderBottom: "none" }} />
+                                                    <img src={img2} alt="" style={{ border: "1px solid black", borderBottom: "none" }} />
+                                                </div>
+                                            </div>
+                                            <div className='writeUpContent'>
+                                                <p>Our gantries represent the latest and most premium innovation in Ahmedabad's outdoor advertising industry. Designed to advertise to both sides of traffic, these large-format, high-impact, overhead billboards inevitably capture the attention of commuters from a distance. Strategically placed in the centre of high-traffic roads and areas, at direct eye level, our gantries ensure maximum visibility and reach to a masses.</p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4>Gantry</h4>
+                                                <div className="small-slite-padding"></div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className='writeUpContent'>
-                                        <p>Our gantries represent the latest and most premium innovation in Ahmedabad's outdoor advertising industry. Designed to advertise to both sides of traffic, these large-format, high-impact, overhead billboards inevitably capture the attention of commuters from a distance. Strategically placed in the centre of high-traffic roads and areas, at direct eye level, our gantries ensure maximum visibility and reach to a masses.</p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4>Gantry</h4>
-                                        <div className="small-slite-padding"></div>
-                                    </div>
-                                </div>
-                                <div className='animated-boxes-six'>
-                                    <div className='writeUpContent'>
-                                        <p>Similar to gantries, cantilevers are overhead billboards placed right in the center of high-traffic roads. Positioned at direct eye level, our cantilevers are impossible to miss for both commuters and pedestrians.</p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4>Cantilever</h4>
-                                        <div className="small-slite-padding"></div>
-                                    </div>
-                                </div>
-                                <div className="animated-boxes-one">
-                                    <div className='writeUpContent'>
-                                        <p>Standing on a single pole, unipoles offer high aesthetic value and convey an elevated brand image. Our unipoles are strategically placed at direct eye level in high-traffic areas, with the sole purpose to avoid clusters and grab eyeballs</p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4>Unipole</h4>
-                                        <div className="small-slite-padding"></div>
+                                    </SwiperSlide>
 
-                                    </div>
-                                </div>
-                                <div className="animated-boxes-two">
-                                    <div className='writeUpContent'>
-                                        <p>These traditional billboards come in medium to large sizes and are supported by multiple poles. Our hoardings are planned to position in a way that provides direct visibility to moving traffic, avoids clusters with other advertisements and ensures clear vision</p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4> Hoarding</h4>
-                                        <div className="small-slite-padding"></div>
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className='animated-boxes-six'>
+                                            <div className='writeUpContent'>
+                                                <p>Similar to gantries, cantilevers are overhead billboards placed right in the center of high-traffic roads. Positioned at direct eye level, our cantilevers are impossible to miss for both commuters and pedestrians.</p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4>Cantilever</h4>
+                                                <div className="small-slite-padding"></div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className="animated-boxes-one">
+                                            <div className='writeUpContent'>
+                                                <p>Standing on a single pole, unipoles offer high aesthetic value and convey an elevated brand image. Our unipoles are strategically placed at direct eye level in high-traffic areas, with the sole purpose to avoid clusters and grab eyeballs</p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4>Unipole</h4>
+                                                <div className="small-slite-padding"></div>
 
-                                    </div>
-                                </div>
-                                <div className="animated-boxes-three">
-                                    <div className='writeUpContent'>
-                                        <p>As pioneers in introducing Pole Kiosks to Ahmedabad, we offer these small-format billboards which are perfect for branding your products and enhancing brand recognition among a broad audience.</p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4> Pole Kiosk</h4>
-                                        <div className="small-slite-padding"></div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className="animated-boxes-two">
+                                            <div className='writeUpContent'>
+                                                <p>These traditional billboards come in medium to large sizes and are supported by multiple poles. Our hoardings are planned to position in a way that provides direct visibility to moving traffic, avoids clusters with other advertisements and ensures clear vision</p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4> Hoarding</h4>
+                                                <div className="small-slite-padding"></div>
 
-                                    </div>
-                                </div>
-                                <div className="animated-boxes-four">
-                                    <div className='writeUpContent'>
-                                        <p>Digital billboards are the up and coming form of outdoor media. With our established LEDs in the top clubs and premier malls of Ahmedabad, get access to premium and large audience. </p>
-                                    </div>
-                                    <div className="back-overlay"></div>
-                                    <div className="content-box">
-                                        <h4> LED</h4>
-                                        <div className="small-slite-padding"></div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className="animated-boxes-three">
+                                            <div className='writeUpContent'>
+                                                <p>As pioneers in introducing Pole Kiosks to Ahmedabad, we offer these small-format billboards which are perfect for branding your products and enhancing brand recognition among a broad audience.</p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4> Pole Kiosk</h4>
+                                                <div className="small-slite-padding"></div>
 
-                                    </div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide style={{ display: "flex" }}>
+                                        <div className="animated-boxes-four">
+                                            <div className='writeUpContent'>
+                                                <p>Digital billboards are the up and coming form of outdoor media. With our established LEDs in the top clubs and premier malls of Ahmedabad, get access to premium and large audience. </p>
+                                            </div>
+                                            <div className="back-overlay"></div>
+                                            <div className="content-box">
+                                                <h4> LED</h4>
+                                                <div className="small-slite-padding"></div>
+
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+
+
+                                </Swiper>
+
+                            </div>
+
+                            {/* <div className='animated-boxes-six'>
+                                <div className='writeUpContent'>
+                                    <p>Similar to gantries, cantilevers are overhead billboards placed right in the center of high-traffic roads. Positioned at direct eye level, our cantilevers are impossible to miss for both commuters and pedestrians.</p>
+                                </div>
+                                <div className="back-overlay"></div>
+                                <div className="content-box">
+                                    <h4>Cantilever</h4>
+                                    <div className="small-slite-padding"></div>
                                 </div>
                             </div>
+                            <div className="animated-boxes-one">
+                                <div className='writeUpContent'>
+                                    <p>Standing on a single pole, unipoles offer high aesthetic value and convey an elevated brand image. Our unipoles are strategically placed at direct eye level in high-traffic areas, with the sole purpose to avoid clusters and grab eyeballs</p>
+                                </div>
+                                <div className="back-overlay"></div>
+                                <div className="content-box">
+                                    <h4>Unipole</h4>
+                                    <div className="small-slite-padding"></div>
+
+                                </div>
+                            </div>
+                            <div className="animated-boxes-two">
+                                <div className='writeUpContent'>
+                                    <p>These traditional billboards come in medium to large sizes and are supported by multiple poles. Our hoardings are planned to position in a way that provides direct visibility to moving traffic, avoids clusters with other advertisements and ensures clear vision</p>
+                                </div>
+                                <div className="back-overlay"></div>
+                                <div className="content-box">
+                                    <h4> Hoarding</h4>
+                                    <div className="small-slite-padding"></div>
+
+                                </div>
+                            </div>
+                            <div className="animated-boxes-three">
+                                <div className='writeUpContent'>
+                                    <p>As pioneers in introducing Pole Kiosks to Ahmedabad, we offer these small-format billboards which are perfect for branding your products and enhancing brand recognition among a broad audience.</p>
+                                </div>
+                                <div className="back-overlay"></div>
+                                <div className="content-box">
+                                    <h4> Pole Kiosk</h4>
+                                    <div className="small-slite-padding"></div>
+
+                                </div>
+                            </div>
+                            <div className="animated-boxes-four">
+                                <div className='writeUpContent'>
+                                    <p>Digital billboards are the up and coming form of outdoor media. With our established LEDs in the top clubs and premier malls of Ahmedabad, get access to premium and large audience. </p>
+                                </div>
+                                <div className="back-overlay"></div>
+                                <div className="content-box">
+                                    <h4> LED</h4>
+                                    <div className="small-slite-padding"></div>
+
+                                </div>
+                            </div> */}
 
 
                             {/* </div> */}
@@ -276,7 +365,7 @@ const MobileHero = () => {
                             </div>
                             <div style={{ width: "100%", zIndex: "4", position: "absolute", bottom: "0px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
                                 <div style={{ paddingTop: "2px" }}>
-                                    <h1 style={{ textAlign: "center", margin: "0px", fontSize: "25px", textTransform: "uppercase" }}><span style={{ color: "#FFED00" }}>Our Key</span> Clients</h1>
+                                    <h1 style={{ textAlign: "center", margin: "0px", fontSize: "25px", textTransform: "uppercase" }}><span style={{ color: "#FFED00" }}>Our Key</span> Clientele</h1>
                                 </div>
                                 <div className="btn-container" style={{ margin: "0px 10px" }}>
                                     <Link to="/OurClientele"><button style={{
--- src/Components/SwiperHeadingIcon/SwiperHeadingIcon.jsx
+++ src/Components/SwiperHeadingIcon/SwiperHeadingIcon.jsx
@@ -5,7 +5,7 @@ import 'swiper/css/pagination';
 import { Autoplay } from 'swiper/modules';
 import MagicHeading from "../MagicHeading/MagicHeading";
 import img from "../InventryManagement/animatedImg.svg"
-const SwiperHeadingIcon = ({headingText} ) => {
+const SwiperHeadingIcon = ({ headingText }) => {
     const imgMap = [
         {
             img: img
@@ -21,29 +21,31 @@ const SwiperHeadingIcon = ({headingText} ) => {
     return (
         <>
             <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
-            <Swiper
-                direction={'vertical'}
+                <div style={{width:"30%"}}>
+                    <Swiper
+                        direction={'vertical'}
 
-                autoplay={{
-                    delay: 2000,
-                    disableOnInteraction: false,
-                }}
-                modules={[Autoplay]}
-                className="mySwiper"
-                loop={true}
-                style={{ height: "10vh", width: "10vh", margin: "0px" }}
-            >
-                {imgMap.map((item, index) => (
-                    <SwiperSlide key={index}>
-                        <img src={item.img} alt="Yellow-Spot Logo" style={{ width: "100%", height: "100%", backgroundColor: "#FFED00", borderRadius: "50%" }} />
-                    </SwiperSlide>
-                ))}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[Autoplay]}
+                        className="mySwiper"
+                        loop={true}
+                        style={{ height: "80px", width: "80px", margin: "0px" }}
+                    >
+                        {imgMap.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <img src={item.img} alt="Yellow-Spot Logo" style={{ width: "100%", height: "100%", backgroundColor: "#FFED00", borderRadius: "50%" }} />
+                            </SwiperSlide>
+                        ))}
 
-            </Swiper>
-            <div style={{ marginTop: "-30px", marginLeft: "30px" }}>
-                <MagicHeading text={headingText} />
-            </div>
-        </div >
+                    </Swiper>
+                </div>
+                <div style={{ marginTop: "-30px",marginLeft:"10px" }}>
+                    <MagicHeading text={headingText} />
+                </div>
+            </div >
         </>
     )
 }
